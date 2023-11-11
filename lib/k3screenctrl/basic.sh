3758381a06000562739f83723511e56452a2384e
Eric-Joker <60310368+Eric-Joker@users.noreply.github.com>
2023-11-11 08:42:19 +0800

    修正uci读取的配置项

--- lib/k3screenctrl/basic.sh
+++ lib/k3screenctrl/basic.sh
@@ -5,7 +5,7 @@
 PRODUCT_NAME_FULL=$(cat /etc/board.json | jsonfilter -e "@.model.name")
 PRODUCT_NAME=${PRODUCT_NAME_FULL#* } # Remove first word to save space
 
-WAN_IFNAME=$(uci get network.wan.ifname)
+WAN_IFNAME=$(uci get network.wan.device)
 MAC_ADDR=$(ifconfig $WAN_IFNAME | grep -oE "([0-9A-Z]{2}:){5}[0-9A-Z]{2}")
 
 CPU_TEMP=$(($(cat /sys/class/thermal/thermal_zone0/temp) / 1000))
